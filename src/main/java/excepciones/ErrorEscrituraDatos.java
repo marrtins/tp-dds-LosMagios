0101f860a3776686d17dae47a6f0372aa512c5d6
derecholuciano <derecholuciano@users.noreply.github.com>
2017-06-12 18:21:05 -0300

    Excepcion escribir datos

--- src/main/java/excepciones/ErrorEscrituraDatos.java
+++ src/main/java/excepciones/ErrorEscrituraDatos.java
@@ -2,8 +2,8 @@ package excepciones;
 
 import java.io.IOException;
 
-public class ErrorCargaDatos extends IOException{
-	public ErrorCargaDatos(Exception e){
-		super("Error al cargar los datos.", e);
+public class ErrorEscrituraDatos extends IOException{
+	public ErrorEscrituraDatos(Exception e){
+		super("Error al escribir los datos en el archivo. Verifique que la sintaxis sea correcta", e);
 	}
 }

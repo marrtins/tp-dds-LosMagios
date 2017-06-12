918e13d576440bca85391dcd9de2f62e03694e13
derecholuciano <derecholuciano@users.noreply.github.com>
2017-06-12 18:20:26 -0300

    Excepcion escritura de datos

--- src/main/java/excepciones/ErrorEscrituraDatos.java
+++ src/main/java/excepciones/ErrorEscrituraDatos.java
@@ -1,5 +1,9 @@
 package excepciones;
 
-public class ErrorEscrituraDatos {
+import java.io.IOException;
 
+public class ErrorCargaDatos extends IOException{
+	public ErrorCargaDatos(Exception e){
+		super("Error al cargar los datos.", e);
+	}
 }

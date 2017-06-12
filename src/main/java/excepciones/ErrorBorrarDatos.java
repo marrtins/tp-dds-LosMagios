0a599d84e04ea1f217b96759c55a0e0ab4b79fad
derecholuciano <derecholuciano@users.noreply.github.com>
2017-06-12 18:18:57 -0300

    Excepcion borrar datos

--- src/main/java/excepciones/ErrorBorrarDatos.java
+++ src/main/java/excepciones/ErrorBorrarDatos.java
@@ -1,5 +1,9 @@
 package excepciones;
 
-public class ErrorBorrarDatos {
+import java.io.IOException;
 
+public class ErrorBorrarDatos extends IOException{
+	public ErrorBorrarDatos(Exception e){
+		super("Error al intentar borrar los datos. Verifique que existan", e);
+	}
 }

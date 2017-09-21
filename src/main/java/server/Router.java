9495b3a764d8c93571494fdc400ceb1a21ec9f37
kevdorfman <kevdorfman95@gmail.com>
2017-09-21 13:04:10 -0300

    Más paquetes

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -1,1 +1,19 @@
 package server;
+
+
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.utils.BooleanHelper;
+import spark.utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+	public static void configure(){
+		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder
+				.create()
+				.withDefaultHelpers()
+				.withHelper("isTrue", BooleanHelper.isTrue)
+				.build();
+	}
+}
+
+
